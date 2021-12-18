7fc5b3673d1679129bec7a8db17761fc36cf72b6
AlexMaystrenko <maystrenko_alex@mail.ru>
2021-12-18 23:28:27 +0300

    lesson kabzda 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  debugger
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is App component
+      <Rating />
+      <Accordion />
     </div>
   );
 }
 
+function Rating() {
+  debugger
+  return (
+    <div>
+      <Star />
+      <Star />
+      <Star />
+      <Star />
+      <Star />
+    </div>
+  );
+}
+
+function Star() {
+  return (
+    <div>
+      Star
+    </div>
+  );
+}
+
+function Accordion () {
+  debugger
+  return (
+    <div>
+      <h3>Menu</h3>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>        
+      </ul>
+    </div>
+  );
+}
 export default App;

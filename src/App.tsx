86ce577aed708fb012c2c820e03d6d6d94b775ce
AlexMaystrenko <maystrenko_alex@mail.ru>
2021-12-19 01:06:41 +0300

    lesson kabzda 03-props

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,24 @@
 import React from 'react';
 import './App.css';
+import { Accordion } from './components/Accordion/Accordion';
+import { Rating } from './components/Rating/Rating';
 
 const App = () => {
-  debugger
   return (
     <div>
-      <AppTitle />
-      <Rating />
-      <Accordion />
+      <input />
+      <PageTitle title={"This is App component"} />
+      <Rating value={3} />
+      <Accordion title={"Title_1"}/>
+      <Accordion title={"Title_2"}/>
+      <PageTitle title={"My frends"} />
+      <Rating value={4} />
     </div>
   );
 }
 
-function AppTitle() {
-  return <>This is App component</>
+function PageTitle(props: any) {
+  return <div>{props.title}</div>
 }
 
-function Rating() {
-  debugger
-  return (
-    <div>
-      <Star />
-      <Star />
-      <Star />
-      <Star />
-      <Star />
-    </div>
-  );
-}
-
-function Star() {
-  return (
-    <div>
-      Star
-    </div>
-  );
-}
-
-function Accordion () {
-  debugger
-  return (
-    <div>
-      <AccordionTitle />
-      <AccordionBody />
-    </div>
-  );
-}
 export default App;
-
-function AccordionTitle() {
-  return <h3>Menu</h3>
-}
-
-function AccordionBody() {
-  return (
-    <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>        
-      </ul>
-  );
-}
---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,24 @@
+
+export function Accordion (props: any) {
+  debugger
+  return (
+    <div>
+      <AccordionTitle title={props.title}/>
+      <AccordionBody />
+    </div>
+  );
+}
+
+function AccordionTitle(props: any) {
+  return <h3>{props.title}</h3>
+}
+
+function AccordionBody() {
+  return (
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>        
+      </ul>
+  );
+}
